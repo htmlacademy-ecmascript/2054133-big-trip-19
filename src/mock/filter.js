4e745afc683bc0195f22f01c210137b83c361c66
Shurafads <wypafads@gmail.com>
2023-01-03 15:21:28 +0300

    Корректирует объект filter

--- src/mock/filter.js
+++ src/mock/filter.js
@@ -8,7 +8,7 @@ const FilterType = {
 };
 
 const filter = {
-  [FilterType.EVERYTHING]: (points) => points.filter((point) => point),
+  [FilterType.EVERYTHING]: (points) => points,
   [FilterType.FUTURE]: (points) => points.filter((point) => isPointFuture(point.dateFrom)),
   [FilterType.PRESENT]: (points) => points.filter((point) => isPointPresent(point.dateFrom)),
   [FilterType.PAST]: (points) => points.filter((point) => isPointPast(point.dateFrom)),
